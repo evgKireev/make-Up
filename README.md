364f309a30c148476d7b23872ee1eeb2e361c261
evgKireev <bob1_k@mail.ru>
2023-05-13 20:29:27 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+@Created by Evgeniy Kireev
+- 🔗 https://evgkireev.github.io/HIMO
+- 🔗 https://www.linkedin.com/in/evgeniy-kireev-13a966239/
---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+$('.header__slider').slick({
+  accessibility: false,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  arrows: false,
+  infinite: true,
+  fade: true,
+  cssEase: 'linear',
+})
