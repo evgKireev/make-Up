5cf6525e8cbcf3aa19ad761e429f033b7b22109f
evgKireev <bob1_k@mail.ru>
2023-05-15 03:11:57 +0300

    add page portfolio

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,21 @@
+const menuItem = document.querySelectorAll('.nav-item')
+const borderLine = document.querySelectorAll('.border-line')
+
+menuItem.forEach((item) => {
+  item.addEventListener('click', setItem)
+})
+
+function setItem(e) {
+  const curTarget = e.currentTarget
+  menuItem.forEach((item) => item.classList.remove('nav-item--active'))
+  curTarget.classList.add('nav-item--active')
+  curTarget.classList.remove('border-line')
+}
+
 $('.header__slider').slick({
   accessibility: false,
   autoplay: true,
-  autoplaySpeed: 3000,
+  autoplaySpeed: 2000,
   arrows: false,
   infinite: true,
   fade: true,
